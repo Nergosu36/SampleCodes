350c206e4dc73e6617fcd94edadd175f23ade1ea
Nergosu36 <39529730+Nergosu36@users.noreply.github.com>
2020-06-03 00:56:15 +0200

    Completed 2nd task, added enumerable days of weeks, modified inits and returned string

--- Swift/rekru.swift
+++ Swift/rekru.swift
@@ -1,29 +1,59 @@
 import Foundation
 import Glibc
  
+ enum DaysOfAWeek 
+ {
+   case Pon
+   case Wt
+   case Sr
+   case Czw
+   case Pt
+   case Sob
+   case Nd
+}
+
 class CheckListElement: CustomStringConvertible
 {
     var Text: String;
     var IsCompleted: Bool;
+    var DayOfWeek: String;
     init()
     {
-        self.Text = "PlaceholderText";
+        self.Text = "Przyk³adowa czynnoœæ";
         self.IsCompleted = false;
+        self.DayOfWeek = "Poniedzia³ek";
     }
-    init(Text_: String, IsCompleted_: Bool)
+    init(Text_: String, IsCompleted_: Bool, DayOfWeek_: DaysOfAWeek)
     {
         self.IsCompleted = IsCompleted_;
         self.Text = Text_;
+        switch DayOfWeek_
+        {
+            case .Pon:
+                self.DayOfWeek = "Poniedzia³ek";
+            case .Wt:
+                self.DayOfWeek = "Wtorek";
+            case .Sr:
+                self.DayOfWeek = "Œroda";
+            case .Czw:
+                self.DayOfWeek = "Czwartek";
+            case .Pt:
+                self.DayOfWeek = "Pi¹tek";
+            case .Sob:
+                self.DayOfWeek = "Sobota";
+            case .Nd:
+                self.DayOfWeek = "Niedziela";
+        }
     }
     var description: String 
     {
-        var str = "\(self.Text) -> ";
+        var str = "\(self.DayOfWeek) \(self.Text) -> ";
         str += self.IsCompleted ? "Gotowe" : "Do Wykonania";
       return str;
    }
 }
 
-let testObj = CheckListElement(Text_: "PoniedziaÅ‚ek ZrobiÄ‡ pranie", IsCompleted_: false);
+let testObj = CheckListElement(Text_: "Zrobiæ pranie", IsCompleted_: false, DayOfWeek_: DaysOfAWeek.Wt);
 let testObj2 = CheckListElement();
 
 print(testObj.description);
